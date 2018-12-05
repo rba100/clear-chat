d78bcb49456be2238d89916ed50d55c044cf5e05
Robin Anderson <robin@rbanderson.co.uk>
2018-12-05 13:33:10 +0000

    colour gen code

--- ClearChat.Core/Repositories/ColourGenerator.cs
+++ ClearChat.Core/Repositories/ColourGenerator.cs
@@ -12,7 +12,10 @@ namespace ClearChat.Core.Repositories
         {
             if (input == "System") return "000000";
             var random = new Random(input.GetStableHashCode());
-            return ColourFromHue(random.NextDouble());
+            var rgb = ColourHelper.HlsToRgb(random.NextDouble() * 360, 0.4, 0.8);
+            return rgb.red.ToString("X").PadLeft(2, '0').ToUpperInvariant() +
+                   rgb.green.ToString("X").PadLeft(2, '0').ToUpperInvariant() +
+                   rgb.blue.ToString("X").PadLeft(2, '0').ToUpperInvariant();
         }
 
         public bool ValidColour(string colourStr, out string errorMessage)
@@ -42,45 +45,6 @@ namespace ClearChat.Core.Repositories
             }
             errorMessage = string.Empty;
             return true;
-        }
-
-        private string ColourFromHue(double hue)
-        {
-            var rgb = HslToRgb(hue, 0.9, 0.30);
-            var value = ((int)rgb[0]).ToString("X").PadLeft(2, '0').ToUpperInvariant() +
-                        ((int)rgb[1]).ToString("X").PadLeft(2, '0').ToUpperInvariant() +
-                        ((int)rgb[2]).ToString("X").PadLeft(2, '0').ToUpperInvariant();
-            return value;
-        }
-
-        private double[] HslToRgb(double h, double s, double l)
-        {
-            double r, g, b;
-
-            if (s == 0)
-            {
-                r = g = b = l; // achromatic
-            }
-            else
-            {
-                var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-                var p = 2 * l - q;
-                r = hue2rgb(p, q, h + 1.0 / 3.0);
-                g = hue2rgb(p, q, h);
-                b = hue2rgb(p, q, h - 1.0 / 3.0);
-            }
-
-            return new[] { Math.Round(r * 255), Math.Round(g * 255), Math.Round(b * 255) };
-        }
-
-        double hue2rgb(double p, double q, double t)
-        {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1.0 / 6) return p + (q - p) * 6 * t;
-            if (t < 1.0 / 2) return q;
-            if (t < 2.0 / 3) return p + (q - p) * (2.0 / 3.0 - t) * 6.0;
-            return p;
         }
     }
 }
